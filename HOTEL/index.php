19b3b822ca3df3c2cffdbf5622403466aa37bff4
DECQ04 <35268239+DECQ04@users.noreply.github.com>
2019-05-27 01:38:38 -0500

    LUNES FINAL

--- HOTEL/index.php
+++ HOTEL/index.php
@@ -4,7 +4,7 @@ require_once "models/enlaces.php";
 require_once "models/crud.php";
 require_once "controllers/controller.php";
 
-$mvc = new MvcController();
-$mvc -> pagina();
+$mvc = new MvcController();//INSTANCIA DE MvcController para usar la funcion pagina()
+$mvc -> pagina();// muestra el template de views , que a su ves instancia un objeto tipo MvcController para usar una fuincion llamada enlacesPaginasController() que muestra la pagina que se requiera con una funcion en model para seleccionar lo que vera el usuario
 
 ?>
