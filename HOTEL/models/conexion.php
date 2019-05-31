27019644d2a45c5f62ebc50c8ea3b12b67fd2c94
DECQ04 <35268239+DECQ04@users.noreply.github.com>
2019-05-30 20:59:17 -0500

    new

--- HOTEL/models/conexion.php
+++ HOTEL/models/conexion.php
@@ -5,7 +5,7 @@ class Conexion{//clase para hacer la conexion a la bd, se ereda para usarse cuan
 	public function conectar(){
 
 		//$link = new PDO("mysql:host=localhost;dbname=id9672334_hotel","id9672334_root","contrasena");
-		$link = new PDO("mysql:host=localhost;dbname=hotel","root","");
+		$link = new PDO("mysql:host=localhost;dbname=hotel","daniel","Cervantes04");
 		return $link;//regresa el string
 
 	}
